f95dc369cab8e0c0ed45d6ea42398f8687a79570
Thanigai <thanigai@excrin.com>
2021-03-14 13:00:50 +0530

    header menu

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import BrandLogo from "../images/logo.svg"
 import { Link } from "gatsby"
 
 const Header = () => {
+  let [toggleMobileMenu, setMenuToggle] = useState(false)
+
+  function handleMobileMenuToggle() {
+    setMenuToggle(!toggleMobileMenu)
+  }
+
   return (
-    <header className="header">
-      <nav className="flex flex-jc-sb flex-ai-c">
+    <header className={toggleMobileMenu ? "header active" : "header"}>
+      <div className="overlay"></div>
+      <nav className="container flex flex-jc-sb flex-ai-c">
         <Link to="/" className="header__logo">
           <img src={BrandLogo} alt="logo" />
         </Link>
-        <a href="#" className="header__menu hide-for-desktop">
+        <div
+          className="header__toggle hide-for-desktop"
+          onClick={handleMobileMenuToggle}
+        >
           <span></span>
           <span></span>
           <span></span>
-        </a>
+        </div>
         <div className="header__links hide-for-mobile">
           <Link to="/" activeClassName="active">
             Home
@@ -27,6 +37,15 @@ const Header = () => {
           Request Invite
         </Link>
       </nav>
+      <div className="header__menu">
+        <Link to="/" activeClassName="active">
+          Home
+        </Link>
+        <Link activeClassName="active">About</Link>
+        <Link activeClassName="active">Contact</Link>
+        <Link activeClassName="active">Blog</Link>
+        <Link activeClassName="active">Careers</Link>
+      </div>
     </header>
   )
 }
