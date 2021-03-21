b531dcb8aed73997b1d44eeef9094ec2f72769e2
Thanigai <thanigai@excrin.com>
2021-03-21 14:23:11 +0530

    grid changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -153,107 +153,107 @@ const IndexPage = () => (
           </h2>
         </div>
         <div class="article__grid container">
-          <a
-            href="#"
-            class="article__item"
+          <div
             data-sal="slide-up"
             data-sal-delay="100"
             data-sal-easing="ease"
             data-sal-duration="800"
           >
-            <div
-              class="article__image"
-              style={{
-                backgroundImage: `url(${article1})`,
-              }}
-            ></div>
-            <div class="article__text">
-              <div class="article__author">By Claire Robinson</div>
-              <div class="article__title">
-                Receive money in any currency with no fees
+            <a href="#" class="article__item">
+              <div
+                class="article__image"
+                style={{
+                  backgroundImage: `url(${article1})`,
+                }}
+              ></div>
+              <div class="article__text">
+                <div class="article__author">By Claire Robinson</div>
+                <div class="article__title">
+                  Receive money in any currency with no fees
+                </div>
+                <div class="article__description">
+                  The world is getting smaller and we’re becoming more mobile.
+                  So why should you be forced to only receive money in a single
+                  …
+                </div>
               </div>
-              <div class="article__description">
-                The world is getting smaller and we’re becoming more mobile. So
-                why should you be forced to only receive money in a single …
-              </div>
-            </div>
-          </a>
-          <a
+            </a>
+          </div>
+          <div
             data-sal="slide-up"
             data-sal-delay="300"
             data-sal-easing="ease"
             data-sal-duration="800"
-            href="#"
-            class="article__item"
           >
-            <div
-              class="article__image"
-              style={{
-                backgroundImage: `url(${article2})`,
-              }}
-            ></div>
-            <div class="article__text">
-              <div class="article__author">By Wilson Hutton</div>
-              <div class="article__title">
-                Treat yourself without worrying about money
+            <a href="#" class="article__item">
+              <div
+                class="article__image"
+                style={{
+                  backgroundImage: `url(${article2})`,
+                }}
+              ></div>
+              <div class="article__text">
+                <div class="article__author">By Wilson Hutton</div>
+                <div class="article__title">
+                  Treat yourself without worrying about money
+                </div>
+                <div class="article__description">
+                  Our simple budgeting feature allows you to separate out your
+                  spending and set realistic limits each month. That means you …
+                </div>
               </div>
-              <div class="article__description">
-                Our simple budgeting feature allows you to separate out your
-                spending and set realistic limits each month. That means you …
-              </div>
-            </div>
-          </a>
-          <a
+            </a>
+          </div>
+          <div
             data-sal="slide-up"
             data-sal-delay="500"
             data-sal-easing="ease"
             data-sal-duration="800"
-            href="#"
-            class="article__item"
           >
-            <div
-              class="article__image"
-              style={{
-                backgroundImage: `url(${article3})`,
-              }}
-            ></div>
-            <div class="article__text">
-              <div class="article__author">By Wilson Hutton</div>
-              <div class="article__title">
-                Take your Easybank card wherever you go
+            <a href="#" class="article__item">
+              <div
+                class="article__image"
+                style={{
+                  backgroundImage: `url(${article3})`,
+                }}
+              ></div>
+              <div class="article__text">
+                <div class="article__author">By Wilson Hutton</div>
+                <div class="article__title">
+                  Take your Easybank card wherever you go
+                </div>
+                <div class="article__description">
+                  Our simple budgeting feature allows you to separate out your
+                  spending and set realistic limits each month. That means you …
+                </div>
               </div>
-              <div class="article__description">
-                We want you to enjoy your travels. This is why we don’t charge
-                any fees on purchases while you’re abroad. We’ll even show you …
-              </div>
-            </div>
-          </a>
-          <a
+            </a>
+          </div>
+          <div
             data-sal="slide-up"
             data-sal-delay="700"
             data-sal-easing="ease"
             data-sal-duration="800"
-            href="#"
-            class="article__item"
           >
-            <div
-              class="article__image"
-              style={{
-                backgroundImage: `url(${article4})`,
-              }}
-            ></div>
-            <div class="article__text">
-              <div class="article__author">By Claire Robinson</div>
-              <div class="article__title">
-                Our invite-only Beta accounts are now live!
+            <a href="#" class="article__item">
+              <div
+                class="article__image"
+                style={{
+                  backgroundImage: `url(${article4})`,
+                }}
+              ></div>
+              <div class="article__text">
+                <div class="article__author">By Claire Robinson</div>
+                <div class="article__title">
+                  Our invite-only Beta accounts are now live!
+                </div>
+                <div class="article__description">
+                  Our simple budgeting feature allows you to separate out your
+                  spending and set realistic limits each month. That means you …
+                </div>
               </div>
-              <div class="article__description">
-                After a lot of hard work by the whole team, we’re excited to
-                launch our closed beta. It’s easy to request an invite through
-                the site …
-              </div>
-            </div>
-          </a>
+            </a>
+          </div>
         </div>
       </div>
     </section>
